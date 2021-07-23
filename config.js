5da3a2dde93cdde3507c104e2f5251de1827aee2
屈轩 <admin@quxuan.org>
2021-07-23 19:25:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ const MO_FISH_PASSWORD = process.env.MO_FISH_PASSWORD
 
 // JueJin Cookie
 const JUE_JIN_COOKIE = process.env.JUE_JIN_COOKIE
+const JUE_JIN_COOKIE2 = process.env.JUE_JIN_COOKIE2
 
 // server酱推送
 const SERVER_J = process.env.SERVER_J
@@ -14,5 +15,6 @@ module.exports = {
   MO_FISH_USERNAME,
   MO_FISH_PASSWORD,
   JUE_JIN_COOKIE,
+  JUE_JIN_COOKIE2,
   SERVER_J,
 }
